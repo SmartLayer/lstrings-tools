fdb1269d18cabb1670f7ad204a9149e21d4a80a6
Weiwu Zhang <a@colourful.land>
2017-07-01 23:04:58 +1000

    this is the first step, convert lstrings to tsv format.
    Merging lstrings file feature will be added later

---
+++ lstrings2tsv.go
@@ -0,0 +1,72 @@
+package main
+
+// This is also a learning project for using file / buffer in golang
+// WW Z
+
+import (
+	"os"
+	"io"
+	"encoding/binary"
+	"fmt"
+	"log"
+	"bytes"
+)
+
+
+func readHeader(file io.Reader) (int32, int32){
+     var count int32
+     var dataSize int32
+     err := binary.Read(file, binary.LittleEndian, &count)
+     if err != nil {
+	fmt.Println("binary.Read failed:", err)
+     }
+     binary.Read(file, binary.LittleEndian, &dataSize)
+     return count, dataSize
+}
+
+
+func main() {
+     var id [65536]int32
+     var offset [65536]int32
+
+     if len(os.Args) != 2 || os.Args[1] == "-h" {
+     	fmt.Println("Convert .ilstring or .dlstring to tab-separated-vector format.")
+	fmt.Println("Provide a single file as input. (This is a golang learning project.)")
+	return
+     }
+     
+     filename := os.Args[1]
+     file, err := os.Open(filename)
+     log.Println("Reading", filename)
+     if err != nil {
+     	log.Fatal(err)
+     }
+     count, dataSize := readHeader(file)
+     log.Println("The file has", count, "records,")
+     log.Println("Consisting", dataSize, "bytes")
+
+     var i int32
+     for i = 0; i < count; i++ {
+     	 binary.Read(file, binary.LittleEndian, &id[i])
+	 binary.Read(file, binary.LittleEndian, &offset[i])
+     }
+     
+     data := make([]byte, dataSize)
+     n, err := file.Read(data)
+     if int32(n) != dataSize {
+     	log.Fatal(err)
+     }
+
+     reader := bytes.NewReader(data)
+
+     buffer := make([]byte, 1024)
+     var length int32
+     for i = 0; i < count; i++ {
+          reader.Seek(int64(offset[i]),0)
+	  binary.Read(reader, binary.LittleEndian, &length)
+	  length-- // last byte is \0
+	  reader.Read(buffer)
+	  fmt.Println(id[i], string(buffer[:length]))
+     }
+}
+
